z3ntu/libqopenrazer

Implement getWaveDirection()

include/libopenrazer/led.h
@@ -56,6 +56,11 @@ public:
     virtual QVector<::razer_test::RGB> getCurrentColors() = 0;
 
     /*!
+     * Returns the wave direction of this Led
+     */
+    virtual ::razer_test::WaveDirection getWaveDirection() = 0;
+
+    /*!
      * Returns the Led ID of this Led
      */
     virtual ::razer_test::RazerLedId getLedId() = 0;
@@ -158,6 +163,7 @@ public:
     bool hasFx(::razer_test::RazerEffect fx) override;
     ::razer_test::RazerEffect getCurrentEffect() override;
     QVector<::razer_test::RGB> getCurrentColors() override;
+    ::razer_test::WaveDirection getWaveDirection() override;
     ::razer_test::RazerLedId getLedId() override;
     bool setOff() override;
     bool setOn() override;
@@ -193,6 +199,7 @@ public:
     bool hasFx(::razer_test::RazerEffect fx) override;
     ::razer_test::RazerEffect getCurrentEffect() override;
     QVector<::razer_test::RGB> getCurrentColors() override;
+    ::razer_test::WaveDirection getWaveDirection() override;
     ::razer_test::RazerLedId getLedId() override;
     bool setOff() override;
     bool setOn() override;

src/razer_test/led.cpp
@@ -104,6 +104,11 @@ QVector<::razer_test::RGB> Led::getCurrentColors()
     return handleDBusVariant<QVector<::razer_test::RGB>>(reply, d->ledIface()->lastError(), Q_FUNC_INFO);
 }
 
+::razer_test::WaveDirection Led::getWaveDirection()
+{
+    return ::razer_test::WaveDirection::RIGHT_TO_LEFT; // TODO Needs implementation
+}
+
 ::razer_test::RazerLedId Led::getLedId()
 {
     QVariant reply = d->ledIface()->property("LedId");

z3ntu/libopenrazer

include/libopenrazer/led.h
@@ -44,6 +44,11 @@ public:
     virtual QVector<::openrazer::RGB> getCurrentColors() = 0;
 
     /*!
+     * Returns the wave direction of this Led
+     */
+    virtual ::openrazer::WaveDirection getWaveDirection() = 0;
+
+    /*!
      * Returns the Led ID of this Led
      */
     virtual ::openrazer::RazerLedId getLedId() = 0;
@@ -174,6 +179,7 @@ public:
     bool hasFx(::openrazer::RazerEffect fx) override;
     ::openrazer::RazerEffect getCurrentEffect() override;
     QVector<::openrazer::RGB> getCurrentColors() override;
+    ::openrazer::WaveDirection getWaveDirection() override;
     ::openrazer::RazerLedId getLedId() override;
     void setOff() override;
     void setOn() override;
@@ -213,6 +219,7 @@ public:
     bool hasFx(::openrazer::RazerEffect fx) override;
     ::openrazer::RazerEffect getCurrentEffect() override;
     QVector<::openrazer::RGB> getCurrentColors() override;
+    ::openrazer::WaveDirection getWaveDirection() override;
     ::openrazer::RazerLedId getLedId() override;
     void setOff() override;
     void setOn() override;

src/razer_test/led.cpp
@@ -87,6 +87,11 @@ QVector<::openrazer::RGB> Led::getCurrentColors()
     return handleDBusVariant<QVector<::openrazer::RGB>>(reply, d->ledIface()->lastError(), Q_FUNC_INFO);
 }
 
+::openrazer::WaveDirection Led::getWaveDirection()
+{
+    return ::openrazer::WaveDirection::RIGHT_TO_LEFT; // TODO Needs implementation
+}
+
 ::openrazer::RazerLedId Led::getLedId()
 {
     QVariant reply = d->ledIface()->property("LedId");