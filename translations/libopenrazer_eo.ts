dce3e1c21720f0a962d869d93b04e8de5c6f4e5c
Luca Weiss <luca@lucaweiss.eu>
2025-04-29 20:35:54 +0200

    Add Esperanto translations

---
+++ translations/libopenrazer_eo.ts
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eo_001">
+<context>
+    <name>libopenrazer</name>
+    <message>
+        <location filename="../include/libopenrazer.h" line="32"/>
+        <source>Off</source>
+        <translation>Malŝaltita</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="33"/>
+        <source>On</source>
+        <translation>Ŝaltita</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="34"/>
+        <source>Static</source>
+        <translation>Statika</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="35"/>
+        <location filename="../include/libopenrazer.h" line="38"/>
+        <source>Breathing</source>
+        <translation>Spirada</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="36"/>
+        <source>Breathing Dual</source>
+        <translation>Spirada duobla</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="37"/>
+        <source>Breathing Random</source>
+        <translation>Spirada hazarda</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="39"/>
+        <source>Blinking</source>
+        <translation>Pulsa</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="40"/>
+        <source>Spectrum</source>
+        <translation>Spektro</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="41"/>
+        <source>Wave</source>
+        <translation>Ondo</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="42"/>
+        <source>Wheel</source>
+        <translation>Rado</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="43"/>
+        <source>Reactive</source>
+        <translation>Reaktiva</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="44"/>
+        <source>Ripple</source>
+        <translation>Ondeto</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="45"/>
+        <source>Ripple Random</source>
+        <translation>Ondeto hazarda</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="56"/>
+        <source>Scroll Wheel</source>
+        <translation>Rulumrado</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="57"/>
+        <source>Battery</source>
+        <translation>Baterio</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="58"/>
+        <source>Logo</source>
+        <translation>Emblemo</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="59"/>
+        <source>Backlight</source>
+        <translation>Fona lumo</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="60"/>
+        <source>Macro Recording</source>
+        <translation>Makroo registrado</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="61"/>
+        <source>Game Mode</source>
+        <translation>Luda reĝimo</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="62"/>
+        <source>Keymap Red</source>
+        <translation>Klavo asigno ruĝa</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="63"/>
+        <source>Keymap Green</source>
+        <translation>Klavo asigno verda</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="64"/>
+        <source>Keymap Blue</source>
+        <translation>Klavo asigno blua</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="65"/>
+        <source>Right Side</source>
+        <translation>Dekstra flanko</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="66"/>
+        <source>Left Side</source>
+        <translation>Maldekstra flanko</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="67"/>
+        <source>Charging</source>
+        <translation>Ŝargado</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="68"/>
+        <source>Fast Charging</source>
+        <translation>Rapida ŝargado</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="69"/>
+        <source>Fully Charged</source>
+        <translation>Plene ŝargita</translation>
+    </message>
+</context>
+</TS>
