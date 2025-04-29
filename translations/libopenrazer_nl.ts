4cef25152a70992463981e4ec2eb28f77fbd0651
Luca Weiss <luca@lucaweiss.eu>
2025-04-29 20:59:53 +0200

    Add Dutch translations

---
+++ translations/libopenrazer_nl.ts
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>libopenrazer</name>
+    <message>
+        <location filename="../include/libopenrazer.h" line="32"/>
+        <source>Off</source>
+        <translation>Uit</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="33"/>
+        <source>On</source>
+        <translation>Aan</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="34"/>
+        <source>Static</source>
+        <translation>Statisch</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="35"/>
+        <location filename="../include/libopenrazer.h" line="38"/>
+        <source>Breathing</source>
+        <translation>Ademen</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="36"/>
+        <source>Breathing Dual</source>
+        <translation>Ademen dubbel</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="37"/>
+        <source>Breathing Random</source>
+        <translation>Ademen toevallig</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="39"/>
+        <source>Blinking</source>
+        <translation>Knipperen</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="40"/>
+        <source>Spectrum</source>
+        <translation>Spectrum</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="41"/>
+        <source>Wave</source>
+        <translation>Golf</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="42"/>
+        <source>Wheel</source>
+        <translation>Wiel</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="43"/>
+        <source>Reactive</source>
+        <translation>Reactief</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="44"/>
+        <source>Ripple</source>
+        <translation>Rimpel</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="45"/>
+        <source>Ripple Random</source>
+        <translation>Rimpel toevallig</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="56"/>
+        <source>Scroll Wheel</source>
+        <translation>Scrollwiel</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="57"/>
+        <source>Battery</source>
+        <translation>Batterij</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="58"/>
+        <source>Logo</source>
+        <translation>Logo</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="59"/>
+        <source>Backlight</source>
+        <translation>Achtergrondverlichting</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="60"/>
+        <source>Macro Recording</source>
+        <translation>Macro-opname</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="61"/>
+        <source>Game Mode</source>
+        <translation>Spelmodus</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="62"/>
+        <source>Keymap Red</source>
+        <translation>Toetsenbordindeling rood</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="63"/>
+        <source>Keymap Green</source>
+        <translation>Toetsenbordindeling groen</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="64"/>
+        <source>Keymap Blue</source>
+        <translation>Toetsenbordindeling blauw</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="65"/>
+        <source>Right Side</source>
+        <translation>Rechterkant</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="66"/>
+        <source>Left Side</source>
+        <translation>Linkerkant</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="67"/>
+        <source>Charging</source>
+        <translation>Opladen</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="68"/>
+        <source>Fast Charging</source>
+        <translation>Snel opladen</translation>
+    </message>
+    <message>
+        <location filename="../include/libopenrazer.h" line="69"/>
+        <source>Fully Charged</source>
+        <translation>Volledig opgeladen</translation>
+    </message>
+</context>
+</TS>
